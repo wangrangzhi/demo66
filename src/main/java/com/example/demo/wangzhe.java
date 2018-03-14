cb6b45c999ee89b74e89aedddf2e83f8652af9b2
wangrangzhi <451155928@qq.com>
2018-03-14 20:55:36 +0800

    add

---
+++ src/main/java/com/example/demo/wangzhe.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import lombok.Data;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@Log
+@Data
+public class wangzhe {
+    private  int a;
+
+
+}
---
+++ src/test/java/com/example/demo/wangzhetest.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+
+import lombok.Data;
+import lombok.extern.java.Log;
+
+@Log
+public @Data
+class wangzhetest {
+
+    wangzhe w ;
+
+    public wangzhetest(wangzhe w) {
+        this.w = w;
+        wangzhe wr = new wangzhe();
+
+
+    }
+
+
+
+}
